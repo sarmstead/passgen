c7a9204ce595a43d1b1585294744b9d17e585981
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-07-09 15:12:42 -0600

    Add options for length, save, numbers and symbols

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const program = require('commander');
 
 program.version('0.1.0').description('Simple Password Generator');
+
+program
+    .option('-l, --length <number>', 'length of password', '8')
+    .option('-s, --save', 'save password to passwords.txt')
+    .option('-nn, --no-numbers', 'remove numbers') // The number option is true by default
+    .option('-ns, --no-symbols', 'remove symbols') // The symbol option is true by default
+    .parse();
+
+console.log(program.opts());
