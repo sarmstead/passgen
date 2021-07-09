fbef906484165449f1d8d0482578abebdc91f6a6
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-07-09 15:29:33 -0600

    Create createPassword function. Initialize chars and use ternary logic to init hasNumbers and hasSymbols.

--- utils/createPassword.js
+++ utils/createPassword.js
@@ -1,3 +1,9 @@
 const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '0123456789';
 const symbols = `!@#$%^&*()_+=-{}][":;'?><,.~]`;
+
+const createPassword = (length = 8, hasNumbers = true, hasSymbols = true) => {
+    let chars = alpha;
+    hasNumbers ? (chars += numbers) : '';
+    hasSymbols ? (chars += symbols) : '';
+}
