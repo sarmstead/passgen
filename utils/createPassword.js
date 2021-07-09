8e9692918ffdeb9cbd7da448c31a5d6b81b94baf
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-07-09 15:35:20 -0600

    Create generatedPassword function

--- utils/createPassword.js
+++ utils/createPassword.js
@@ -2,6 +2,10 @@ const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '0123456789';
 const symbols = `!@#$%^&*()_+=-{}][":;'?><,.~]`;
 
+const generatedPassword = (length, chars) => {
+    
+}
+
 const createPassword = (length = 8, hasNumbers = true, hasSymbols = true) => {
     let chars = alpha;
     hasNumbers ? (chars += numbers) : '';
